fb306d4a83d6e7842cf9b37e7ae2335f36684cfa
Steve Marple <s.marple@lancaster.ac.uk>
2017-01-31 09:54:59 +0000

    Remove execute permission from license.txt

--- fix_file_permissions.sh
+++ fix_file_permissions.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-DIR=`dirname $0`
-if [ -z "$DIR" ]; then
+DIR="$1"
+if [ -z "$DIR" -o ! -d "$DIR" ]; then
     echo "Unknown directory"
     exit 1
 fi
-DIR="$DIR/SDK"
+DIR=`readlink -f "$1"`
+
+echo "Fixing permissions on  $DIR"
+sleep 2
 
 # Remove write for everyone
 chmod -R o-w "$DIR"
 
 # Remove execute from files which are not in a bin directory
 find "$DIR" -type f -not -regex '.*/bin/.*' -exec chmod a-x {} +
+
+# Remove execute from files in the root of the directory
+# find "$DIR" -type f -not -regex '.*' -exec chmod a-x {} +
+find "$DIR" -maxdepth 1 -type f -regex '.*' -exec chmod a-x {} +
+
+echo "Done"
